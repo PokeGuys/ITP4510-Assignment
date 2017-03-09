7b5fca83b8beadfbba9a5b573332e57a3fb21012
pokeguys <pokeguys@users.noreply.github.com>
2017-03-09 10:22:12 -0600

    Update IntegerComparator.java

--- IntegerComparator.java
+++ IntegerComparator.java
@@ -1,11 +1,3 @@
-public interface Comparator {
-  public boolean isEqualTo(Object item1, Object item2);
-  public boolean isGreaterThan(Object item1, Object item2);
-  public boolean isGreaterThanOrEqualTo(Object item1, Object item2);
-  public boolean isLessThan(Object item1, Object item2);
-  public boolean isLessThanOrEqualTo(Object item1, Object item2);
-}
-
 public class IntegerComparator implements Comparator {
   public boolean isEqualTo(Object item1, Object item2) {
     if ((int)item1 == (int)item2) {
