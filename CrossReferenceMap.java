beef218317299a8c76e4c8ae97b72dfa8efb7174
pokeguys <pokeguys@users.noreply.github.com>
2017-03-09 10:21:06 -0600

    Update CrossReferenceMap.java

--- CrossReferenceMap.java
+++ CrossReferenceMap.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class CrossReferenceMap {
+  private static boolean startString = false;
   private static final String[] reserved = {
     "abstract", "boolean", "break", "byte", "case", "catch", "char",
     "class", "const", "continue", "default", "do", " double", "else",
@@ -21,7 +22,7 @@ public class CrossReferenceMap {
     BinarySearchTree tree = new BinarySearchTree();
     BufferedReader source;
     String line;
-    int current_line = 0;
+    int current_line = 1;
     for (String word : reserved) {
       dictionary.insert(word);
     }
@@ -30,13 +31,16 @@ public class CrossReferenceMap {
       source = new BufferedReader(new FileReader(args[0]));
       line = source.readLine();
       while (line != null) {
-        StringTokenizer st1 = new StringTokenizer(line," _.,;()[]{}+-*/!=<>\t");
+        StringTokenizer st1 = new StringTokenizer(line," .,;()[]{}+-*/!=<>\t");
         while (st1.hasMoreTokens()) {
           String s1 = st1.nextToken();
           if (!dictionary.search(s1)) {
-            if (!isString(s1) && !isInteger(s1)) {
-              if (!tree.search(s1)) {
-              	tree.insert(s1);
+            if (!isString(s1)) {
+              if (!isInteger(s1) && !isOperator(s1)) {
+                if (!tree.search(s1)) {
+                	tree.insert(s1);
+                }
+                tree.insertLine(s1, current_line);
               }
             }
           }
@@ -46,7 +50,7 @@ public class CrossReferenceMap {
       }
       source.close();
     } catch (IOException iox) {
-      System.out.println("Problem encountered in reading file!" );  
+      System.out.println("Problem encountered in reading file!" );
     }
     System.out.print("Cross Map Reference :- \n");
     tree.inorder();
@@ -54,15 +58,16 @@ public class CrossReferenceMap {
 
   public static boolean isInteger(String word) {
     try {
+      word = word.replace("_", "");
       if (!Character.isDigit(word.charAt(0)))
         return false;
       char lastWord = word.charAt(word.length() - 1);
       if (containLiteral(lastWord))
-        word = word.substring(0, word.length() - 2);
-      if (word.indexOf('_') >= 0)
-        word = word.replace("_", "");
+        word = word.substring(0, word.length() - 1);
       if (word.indexOf('e') >= 0) {
         Double.valueOf(word);
+      } else if (word.indexOf("0b") == 0) {
+        Long.parseLong(word.substring(2), 2);
       } else {
         Long.decode(word);
       }
@@ -75,7 +80,27 @@ public class CrossReferenceMap {
   }
 
   public static boolean isString(String word) {
-    return (word.indexOf('"') >= 0 || word.indexOf("'") >= 0);
+    word = word.replace("\\\"", "");
+    if (!startString) {
+      char firstWord = word.charAt(0);
+      int count = 0;
+      switch (firstWord) {
+        case '"':
+          count = word.length() - word.replace("\"", "").length();
+          break;
+        case '\'':
+          count = word.length() - word.replace("'", "").length();
+          break;
+      }
+      startString = (count == 1);
+      return (count > 0);
+    } else {
+      if (word.indexOf("\"") >= 0 || word.indexOf('\'') >= 0) {
+        startString = false;
+        return true;
+      }
+    }
+    return startString;
   }
 
   public static boolean containLiteral(char word) {
@@ -86,5 +111,11 @@ public class CrossReferenceMap {
         return true;
     }
     return false;
+  }
+
+  protected static boolean isOperator(String op) {
+    return (op.equals("~") || op.equals("%") || op.equals("instanceof") ||
+      op.equals("&") || op.equals("^") || op.equals("|") || op.equals("&&") ||
+      op.equals("||") || op.equals("?") || op.equals(":"));
   }
 }
