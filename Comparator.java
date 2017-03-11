6173c36fefb491bdbbab6028ab6aa9db941aaed7
pokeguys <pokeguys@users.noreply.github.com>
2017-03-11 15:20:00 +0800

    Delete Comparator.java

--- Comparator.java
+++
@@ -1,7 +0,0 @@
-public interface Comparator {
-  public boolean isEqualTo(Object item1, Object item2);
-  public boolean isGreaterThan(Object item1, Object item2);
-  public boolean isGreaterThanOrEqualTo(Object item1, Object item2);
-  public boolean isLessThan(Object item1, Object item2);
-  public boolean isLessThanOrEqualTo(Object item1, Object item2);
-}
