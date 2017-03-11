6031cd7eb9361882f897bc53a5d30b680a14e41e
pokeguys <pokeguys@users.noreply.github.com>
2017-03-11 15:47:59 +0800

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,177 +1,196 @@
-class BinaryNode {
-	Object data;
-	LinkedList lines = new LinkedList();
-	BinaryNode left;
-	BinaryNode right;
+class BinaryNode
+{
+  Object data;
+  BinaryNode left;
+  BinaryNode right;
+  LinkedList lines;
 
-	public BinaryNode(Object d) {
-		data = d;
-		left = right = null;
-	}
-	public Object getData() {
-		return data;
-	}
-	public String getLines() {
-		return lines.toString();
-	}
-} // class BinaryNode
+  public BinaryNode(Object d)
+  {
+    data = d;
+    left = right = null;
+    lines = new LinkedList();
+  }
 
-public class BinarySearchTree {
+  public Object getData()
+  {
+    return data;
+  }
 
-	private BinaryNode root;
+  public String getLines()
+  {
+    return lines.toString();
+  }
+}
 
-	public BinarySearchTree() {
-		root = null;
-	}
+public class BinarySearchTree
+{
+  private BinaryNode root;
 
-	public boolean isEmpty() {
-		return root == null;
-	}
+  public BinarySearchTree()
+  {
+    root = null;
+  }
 
-	public boolean search(Object x) {
-		if (search(root, x) != null)
-			return true;
-		else
-			return false;
-	}
+  public boolean isEmpty()
+  {
+    return root == null;
+  }
 
-	private BinaryNode search(BinaryNode t, Object x) {
-		if (t == null)
-			return null;
-		if (((Comparable)x).compareTo(t.data) < 0)
-			return search(t.left, x);
-		else if (((Comparable)x).compareTo(t.data) > 0)
-				return search(t.right, x);
-			 else
-				return t;
-	}
+  public boolean search(Object x)
+  {
+    if (search(root, x) != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	public void insert(Object x) {
-		root = insertSubtree(root, x);
-	}
+  private BinaryNode search(BinaryNode t, Object x)
+  {
+    if (isEmpty()) return null;
+    if (((Comparable)x).compareTo(t.getData()) < 0) {
+      return search(t.left, x);
+    } else if (((Comparable)x).compareTo(t.getData()) > 0) {
+      return search(t.right, x);
+    }
+    return t;
+  }
 
-	public void insertLine(Object x, int currentLine) {
-		BinaryNode target = search(root, x);
-		target.lines.insertInorder(currentLine);
-	}
+  public void insert(Object x)
+  {
+    root = insertSubtree(root, x);
+  }
 
-	private BinaryNode insertSubtree(BinaryNode t, Object x) {
-		if (t == null)
-			t = new BinaryNode(x);
-		else if (((Comparable)x).compareTo(t.data) < 0)
-			t.left = insertSubtree(t.left, x);
-		else //if (((Comparable)x).compareTo(t.data) > 0)
-			t.right = insertSubtree(t.right, x);
-		return t;
-	}
+  private BinaryNode insertSubtree(BinaryNode t, Object x)
+  {
+    if (t == null) {
+      t = new BinaryNode(x);
+    } else if (((Comparable)x).compareTo(t.getData()) < 0) {
+      t.left = insertSubtree(t.left, x);
+    } else {
+      t.right = insertSubtree(t.right, x);
+    }
+    return t;
+  }
 
-	private void visit(BinaryNode t) {
-		System.out.printf("%-50s : %s\n", t.data, t.getLines());
-	}
+  public void insertLines(Object x, int current)
+  {
+    BinaryNode target = search(root, x);
+    target.lines.insertInorder(current);
+  }
 
-	public void preorder() {
-		preorderSubtree(root);
-		System.out.println();
-	}
+  public void delete(Object x)
+  {
+    root = deleteSubtree(root, x);
+  }
 
-	private void preorderSubtree(BinaryNode t) {
-		if (t == null) return;
-		visit(t);
-		preorderSubtree(t.left);
-		preorderSubtree(t.right);
-	}
+  private BinaryNode deleteSubtree(BinaryNode t, Object x)
+  {
+    if (t == null) return null;
+    if (((Comparable)x).compareTo(t.getData()) < 0) {
+      t.left = deleteSubtree(t.left, x);
+    } else if (((Comparable)x).compareTo(t.getData()) > 0) {
+      t.right = deleteSubtree(t.right, x);
+    } else if (t.left != null && t.right != null) {
+      t.data = findMin(t.right).getData();
+      t.right = deleteSubtree(t.right, t.data);
+    } else {
+      t = (t.left != null) ? t.left : t.right;
+    }
+    return t;
+  }
 
-	public void inorder() {
-		inorderSubtree(root);
-		System.out.println();
-	}
+  private void visit(BinaryNode t)
+  {
+    System.out.println(t.getData());
+  }
 
-	private void inorderSubtree(BinaryNode t) {
-		if (t == null) return;
-		inorderSubtree(t.left);
-		visit(t);
-		inorderSubtree(t.right);
-	}
+  public void preorder()
+  {
+    preorderSubtree(root);
+    System.out.println();
+  }
 
-	public void postorder() {
-		postorderSubtree(root);
-		System.out.println();
-	}
+  private void preorderSubtree(BinaryNode t)
+  {
+    if (t == null) return;
+    visit(t);
+    preorderSubtree(t.left);
+    preorderSubtree(t.right);
+  }
 
-	private void postorderSubtree(BinaryNode t) {
-		if (t == null) return;
-		postorderSubtree(t.left);
-		postorderSubtree(t.right);
-		visit(t);
-	}
+  public void inorder()
+  {
+    inorderSubtree(root);
+    System.out.println();
+  }
 
-	public Object getMin() {
-		return findMin(root).data;
-	}
+  private void inorderSubtree(BinaryNode t)
+  {
+    if (t == null) return;
+    inorderSubtree(t.left);
+    visit(t);
+    inorderSubtree(t.right);
+  }
 
-	private BinaryNode findMin(BinaryNode t) {
-		if (t == null)
-			return null;
-		else if (t.left == null)
-				return t;
-			 else
-				return findMin(t.left);
-	}
+  public void postorder()
+  {
+    postorderSubtree(root);
+    System.out.println();
+  }
 
-	public Object getMax() {
-		return findMax(root).data;
-	}
+  private void postorderSubtree(BinaryNode t)
+  {
+    if (t == null) return;
+    postorderSubtree(t.left);
+    postorderSubtree(t.right);
+    visit(t);
+  }
 
-	private BinaryNode findMax(BinaryNode t) {
-		if (t == null)
-			return null;
-		else if (t.right == null)
-				return t;
-			 else
-				return findMax(t.right);
-	}
+  public Object getMin()
+  {
+    return findMin(root).getData();
+  }
 
-	public void delete(Object x) {
-		root = deleteSubtree(root, x);
-	}
+  private BinaryNode findMin(BinaryNode t)
+  {
+    if (t == null) return t;
+    return (t.left == null ? t : findMin(t.left));
+  }
 
-	private BinaryNode deleteSubtree(BinaryNode t, Object x) {
-		BinaryNode temp, child;
+  public Object getMax()
+  {
+    return findMin(root).getData();
+  }
 
-		if (t == null) return null;
-		if (((Comparable)x).compareTo(t.data) < 0)
-			t.left = deleteSubtree(t.left, x);
-		else if (((Comparable)x).compareTo(t.data) > 0)
-			t.right = deleteSubtree(t.right, x);
-		else if (t.left != null && t.right != null) {
-			/* has 2 leaves */
-			t.data = findMin(t.right).data;
-			t.right = deleteSubtree(t.right, t.data);
-		}
-		else
-			t = (t.left != null) ? t.left : t.right;
+  private BinaryNode findMax(BinaryNode t)
+  {
+    if (t == null) return t;
+    return (t.right == null ? t : findMax(t.right));
+  }
 
-		return t;
-	}
+  public int size()
+  {
+    return sizeSubtree(root);
+  }
 
-	public int size() {
-		return sizeSubtree(root);
-	}
+  private int sizeSubtree(BinaryNode t)
+  {
+    if (t == null) return 0;
+    return sizeSubtree(t.left) + sizeSubtree(t.right) + 1;
+  }
 
-	private int sizeSubtree(BinaryNode t) {
-		if (t == null) return 0;
-		return sizeSubtree(t.left) + sizeSubtree(t.right) + 1;
-	}
+  public int height()
+  {
+    return heightSubtree(root);
+  }
 
-	public int height() {
-		return heightSubtree(root);
-	}
-
-	private int heightSubtree(BinaryNode t) {
-		if (t == null) return -1;
-		int h = heightSubtree(t.left);
-		int k = heightSubtree(t.right);
-		return h > k ? h + 1 : k + 1;
-	}
-
-} // class BinarySearchTree
+  private int heightSubtree(BinaryNode t)
+  {
+    if (t == null) return -1;
+    int left = heightSubtree(t.left);
+    int right = heightSubtree(t.right);
+    return (left > right ? left + 1 : right + 1);
+  }
+}
