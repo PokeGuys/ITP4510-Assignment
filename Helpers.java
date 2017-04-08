688813430d08220133e5abd910d31b246c01b13f
pokeguys <pokeguys@users.noreply.github.com>
2017-04-08 23:25:02 +0800

    Update Helpers.java

--- Helpers.java
+++ Helpers.java
@@ -1,5 +1,27 @@
 public interface Helpers
 {
+  /**
+   * Reserved word used to avoid inserting into tree.
+   *
+   * @var String array
+   */
+  private final String[] RESERVED = {
+    "abstract", "boolean", "break", "byte", "case", "catch", "char",
+    "class", "const", "continue", "default", "do", " double", "else",
+    "extends", "false", "final", "finally", "float", "for", "goto", "if",
+    "implements", "import", " instanceof", "int", "interface", "long", "native",
+    "new", "null", "operator", "package", "private", " protected", "public", "rest",
+    "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this",
+    "throw", "throws", "transient", "true", "try", "void", "volatile", "while"
+  };
+
+  /**
+   * Dictionary storing reserved word.
+   *
+   * @var BinarySearchTree
+   */
+  private BinarySearchTree dictionary = new BinarySearchTree();
+
   public boolean isIdentifier(String item);
   public boolean isString(String item);
 }
