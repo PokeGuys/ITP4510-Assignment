7b6c8aacbee269a0ab28a63a6219690feea2bdb6
pokeguys <pokeguys@users.noreply.github.com>
2017-04-08 23:46:19 +0800

    Update CrossReferenceMap.java

--- CrossReferenceMap.java
+++ CrossReferenceMap.java
@@ -4,109 +4,6 @@ import java.util.*;
 public class CrossReferenceMap
 {
   /**
-   * Reserved word used to avoid inserting into tree.
-   *
-   * @var String array
-   */
-  private final String[] RESERVED = {
-    "abstract", "boolean", "break", "byte", "case", "catch", "char",
-    "class", "const", "continue", "default", "do", " double", "else",
-    "extends", "false", "final", "finally", "float", "for", "goto", "if",
-    "implements", "import", " instanceof", "int", "interface", "long", "native",
-    "new", "null", "operator", "package", "private", " protected", "public", "rest",
-    "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this",
-    "throw", "throws", "transient", "true", "try", "void", "volatile", "while"
-  };
-
-  /**
-   * Used to insert existed word into tree for refernce.
-   *
-   * @var BinarySearchTree
-   */
-  private BinarySearchTree tree = new BinarySearchTree();
-
-  /**
-   * Dictionary storing reserved word.
-   *
-   * @var BinarySearchTree
-   */
-  private BinarySearchTree dictionary = new BinarySearchTree();
-
-  /**
-   * Search helpers.
-   *
-   * @var SearchHelpers
-   */
-  private Helpers helpers = new SearchHelpers();
-
-  /**
-   * Store reserved word into BinaryTree.
-   *
-   * @return void
-   */
-  public CrossReferenceMap()
-  {
-    for (String word : RESERVED) {
-      dictionary.insert(word);
-    }
-  }
-
-  /**
-   * Determine is it identifer by checking
-   * 1. NOT a reserved word.
-   * 2. NOT in a String.
-   * 3. Start with [$_a-zA-Z].
-   *
-   * @param String word
-   *
-   * @return boolean
-   */
-  public boolean isIdentifier(String word)
-  {
-    return (!dictionary.search(word) && !helpers.isString(word) && helpers.isIdentifier(word));
-  }
-
-
-  /**
-   * Determine is it already in tree, if not insert.
-   * Insert currentLine into tree node.
-   *
-   * @param String word
-   * @param int line
-   *
-   * @return void
-   */
-  public void insertTree(String word, int line)
-  {
-    if (!tree.search(word)) {
-      tree.insert(word);
-    }
-    tree.insertLines(word, line);
-  }
-
-  /**
-   * Press any key to continue
-   *
-   * @return String
-   */
-  public void pressAnyKey() {
-    try {
-      System.out.println("Press any key to continue...");
-      System.in.read();
-    } catch(Exception e) {}
-  }
-
-  /**
-   * Print out CrossReferenceMap
-   *
-   * @return void
-   */
-  public void printResult()
-  {
-    tree.inorder();
-  }
-
-  /**
    * CrossMapReference Main Program.
    * Check starting arguments, if length not equal 1, exit.
    * Read the file and parse it into Tokens.
@@ -127,17 +24,29 @@ public class CrossReferenceMap
       System.out.println("Usage: java CrossReferenceMap <filename>");
       System.exit(1);
     }
-    // Initialize dictionary for the reserved word.
-    CrossReferenceMap map = new CrossReferenceMap();
     BufferedReader source;
     String line;
 
+    /**
+     * Used to insert existed word into tree for refernce.
+     *
+     * @var BinarySearchTree
+     */
+    BinarySearchTree tree = new BinarySearchTree();
+
+    /**
+     * Search helpers.
+     *
+     * @var SearchHelpers
+     */
+    Helpers helpers = new SearchHelpers();
+    
     /**
      * Current line.
      *
      * @var Integer
      */
-    int current = 1;
+    int currentLine = 1;
 
     // Prevent any IOException when reading file
     try {
@@ -149,10 +58,20 @@ public class CrossReferenceMap
         // Tokenize whole line into pieces
         StringTokenizer st1 = new StringTokenizer(line," .,;()[]{}+-*/!=<>\t");
         while (st1.hasMoreTokens()) {
-          // Determine is s1 a identifier
+          /**
+           * Determine is s1 a identifer by checking
+           * 1. NOT a reserved word.
+           * 2. NOT in a String.
+           * 3. Start with [$_a-zA-Z].
+           */
           String s1 = st1.nextToken();
-          if (map.isIdentifier(s1)) {
-            map.insertTree(s1, current);
+          if (helpers.isQualified(s1)) {
+            // Determine is it already in tree, if not insert.
+            // Insert currentLine into tree node.
+            if (!tree.search(word)) {
+              tree.insert(word);
+            }
+            tree.insertLines(word, currentLine);
           }
         }
         // Add 1 to currentLine whcih means this line is already finished.
@@ -168,9 +87,12 @@ public class CrossReferenceMap
 
     // Print out the Cross Map Reference
     System.out.print("Cross Map Reference :- \n");
-    map.printResult();
+    tree.inorder();
 
     // Press any key to continue.
-    map.pressAnyKey();
+    try {
+      System.out.println("Press any key to continue...");
+      System.in.read();
+    } catch(Exception e) {}
   }
 }
